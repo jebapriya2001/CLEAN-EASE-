ee1fd973123f36cd6989d21d280effc4a79f242b
jebapriya2001 <jebapriya482001@gmail.com>
2024-11-10 23:29:47 +0530

    Front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import Navbar from './components/Navbar';
+import CategoryList from './components/CategoryList';
+import SearchFilter from './components/SearchFilter';
+import Checklist from './components/Checklist';
+import ScheduleAppointment from './components/ScheduleAppointment';
+import Payment from './components/Payment';
+import Reminders from './components/Reminders';
+import Reviews from './components/Reviews';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<CategoryList />} />
+                <Route path="/search" element={<SearchFilter />} />
+                <Route path="/checklist" element={<Checklist />} />
+                <Route path="/schedule" element={<ScheduleAppointment />} />
+                <Route path="/payment" element={<Payment />} />
+                <Route path="/reminders" element={<Reminders />} />
+                <Route path="/reviews" element={<Reviews />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
